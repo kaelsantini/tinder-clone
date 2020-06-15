b53e76da20e709b7ed0c068980d2f6a7359e985d
Kael Santini <kaelsantini@hotmail.com>
2020-06-15 11:02:41 -0300

    added new features, working...

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :accounts
+
+  get '/browse' => "browse#browse", as: :browse
+
+  
+  
+
   root to: "public#home"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Account.create([
+    { first_name: "Kael", last_name: "Santini", username: "kaelsantini", email: "kael@test.com", password: "123456" },
+    { first_name: "Fulano", last_name: "de Tal", username: "fulanodetal", email: "fulanodetal@test.com", password: "654321" },
+    { first_name: "Ciclano", last_name: "da Silva", username: "ciclanosilva", email: "ciclanos@test.com", password: "anotherpass" },
+    { first_name: "Beltrano", last_name: "dos Santos", username: "beltranosasntos", email: "beltranosantos@test.com", password: "anotheranother" },
+    { first_name: "Mélvio", last_name: "Gonvales", username: "melviog", email: "melviogg@test.com", password: "something" }
+])
